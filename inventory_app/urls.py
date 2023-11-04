a964c24d63829ae4ad552058eb55c180f92dcaba
Angie Restanio <arestani@uccs.edu>
2023-11-04 15:14:40 -0600

    implement create, update and delete item

--- inventory_app/urls.py
+++ inventory_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('items/', views.ItemListView.as_view(), name='items'), 
     path('items/<int:pk>', views.ItemDetailView.as_view(), name='item-detail'),
+    path('items/create_item/', views.createItem, name='create_item'),
+    path('items/update_item/<int:item_id>', views.updateItem, name='update_item'), 
+    path('items/delete_item/<int:item_id>', views.deleteItem, name='delete_item'),
 ]
--- inventory_app/views.py
+++ inventory_app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import *
 from django.views import generic
+from .forms import ItemForm
 
 # Create your views here.
 
@@ -9,7 +10,58 @@ def index(request):
     return render( request, 'inventory_app/index.html')
 
 class ItemListView(generic.ListView):
-    model = Item\
+    model = Item
     
 class ItemDetailView(generic.DetailView):
     model = Item
+
+def createItem(request):
+    form = ItemForm()
+
+    if request.method == 'POST':
+        # create new item
+        form = ItemForm(request.POST)
+
+        if form.is_valid():
+            # save to database
+            form.save()
+
+            # Redirect back to the portfolio detail page
+            return redirect('items')
+    
+    context = {'form': form}
+    return render(request, 'inventory_app/item_form.html', context)
+
+def updateItem(request, item_id):
+    # get specified item
+    item = get_object_or_404(Item, pk=item_id)
+
+    # fill form with specified item's data
+    form = ItemForm(instance=item)
+
+    if request.method == 'POST':
+        # set updated data to specific instance of item
+        form = ItemForm(request.POST, instance=item)
+        
+        if form.is_valid():
+            # save new instance to database
+            form.save()
+
+            # Redirect back to the portfolio detail page
+            return redirect('items')
+    
+    context = {'form': form}
+    return render(request, 'inventory_app/item_form.html', context)
+
+def deleteItem(request, item_id):
+    # get specified item
+    item = get_object_or_404(Item, pk=item_id)
+
+    if request.method == 'POST':
+        item.delete()
+        return redirect('items')
+    
+    context = {'item': item}
+    return render(request, 'inventory_app/item_delete.html', context)
+
+
