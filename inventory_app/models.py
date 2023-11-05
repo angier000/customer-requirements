ff009e9c6feb9645efa9ee73dfdb07be4e4dba37
Angie Restanio <arestani@uccs.edu>
2023-11-05 11:37:33 -0700

    incorporate accessibility and better design

--- inventory_app/models.py
+++ inventory_app/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Item(models.Model):
     name = models.CharField(max_length = 200)
     description = models.TextField(blank=True)
-    serial_number = models.CharField(max_length=100)
+    serial_number = models.CharField(max_length=100, blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = models.ImageField(upload_to='uploads/', null=True, blank=True)
     # inventory = models.ForeignKey(Inventory, on_delete=models.CASCADE, default = None)
