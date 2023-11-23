e3c37845d8268d4debf1d3b861d8ac6cee1f2fe9
Angie Restanio <arestani@uccs.edu>
2023-11-23 03:46:45 -0700

    add chromedriver to folder

--- inventory_app/tests/test_selenium.py
+++ inventory_app/tests/test_selenium.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
 from django.test import LiveServerTestCase
+import os
 
 
 class Hosttest(LiveServerTestCase):
     def setUp(self):
+        #chrome_driver_path = "../chromedriver"
+        #chrome_driver_path = os.path.join(os.getcwd(), "chromedriver")
+        #os.environ["PATH"] += os.pathsep + chrome_driver_path
+        #self.driver = webdriver.Chrome(chrome_driver_path)
+        #os.environ["PATH"] += os.pathsep + chrome_driver_path
         self.driver = webdriver.Chrome()
 
     def test_homepage(self):
-        #self.driver.get(self.live_server_url) # http://127.0.0.1:8000/
+        #self.driver.get(self.live_server_url) 
         self.driver.get('http://127.0.0.1:8000/')
 
         self.assertIn("Inventory Tracker", self.driver.title)
