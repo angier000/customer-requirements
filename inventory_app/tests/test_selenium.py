867470b34ccd4a804177a4f4e62333316dad363a
Angie Restanio <arestani@uccs.edu>
2023-11-23 04:32:33 -0700

    make update

--- inventory_app/tests/test_selenium.py
+++ inventory_app/tests/test_selenium.py
@@ -13,8 +13,8 @@ class Hosttest(LiveServerTestCase):
         self.driver = webdriver.Chrome()
 
     def test_homepage(self):
-        #self.driver.get(self.live_server_url) 
-        self.driver.get('http://127.0.0.1:8000/')
+        self.driver.get(self.live_server_url) 
+        #self.driver.get('http://127.0.0.1:8000/')
 
         self.assertIn("Inventory Tracker", self.driver.title)
         #self.assertIn(1, [1,2,3])
