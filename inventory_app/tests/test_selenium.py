04d1d37824dcd4d08bec17422abf87f6d66e5436
Angie Restanio <arestani@uccs.edu>
2023-11-23 20:18:24 -0700

    write selenium login and logout test

--- inventory_app/tests/test_selenium.py
+++ inventory_app/tests/test_selenium.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 from django.test import LiveServerTestCase
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import os
 import time
 
+SERVER_URL = 'http://127.0.0.1:8000/'
+#SERVER_URL = self.live_server_url
 
 class Hosttest(LiveServerTestCase):
     def setUp(self):
@@ -12,15 +16,61 @@ class Hosttest(LiveServerTestCase):
         #self.driver = webdriver.Chrome(chrome_driver_path)
         #os.environ["PATH"] += os.pathsep + chrome_driver_path
         self.driver = webdriver.Chrome()
+        print('debug: ', type(self.driver))
+
+
+    def get_server_url(self):
+        # Check if running in GitHub Actions
+        if os.getenv('GITHUB_ACTIONS'):
+            return self.live_server_url
+        else:
+            return SERVER_URL
+
 
     def test_homepage(self):
-        #self.driver.get(self.live_server_url + ':8000')
-        self.driver.get(self.live_server_url) 
-        #self.driver.get('http://127.0.0.1:8000/')
+        server_url = self.get_server_url()
+        #self.driver.get(self.live_server_url) 
+        self.driver.get(server_url)
 
         self.assertIn("Inventory Tracker", self.driver.title)
         #self.assertIn(1, [1,2,3])
-        time.sleep(5)
+        time.sleep(1)
+
+    def test_login_logout(self):
+        server_url = self.get_server_url()
+
+        #self.driver.get(self.live_server_url + '/accounts/login/')
+        self.driver.get(server_url  + 'accounts/login/')
+        print("Current URL:", self.driver.current_url)
+
+        # define username and p[assword
+        user_name = self.driver.find_element(By.NAME, 'username')
+        user_password = self.driver.find_element(By.NAME, 'password')
+        submit = self.driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
+
+        # log in user
+        user_name.send_keys('testuser')
+        user_password.send_keys('inventorytest')
+        submit.send_keys(Keys.RETURN)
+
+        # check for welcome message ensuring the correct user is logged in
+        message = self.driver.find_element(By.XPATH, '//span[contains(text(), "Welcome, testuser")]')
+        self.assertTrue(message.is_displayed(), "Welcome, testuser")
+
+        # log out user
+        logout = self.driver.find_element(By.XPATH, '//a[@href="/accounts/logout/"]')
+        logout.click()
+
+        # Verify that the user is redirected to the login page
+        login_url = SERVER_URL + 'accounts/login/'
+        self.assertEqual(self.driver.current_url, login_url, "Logout redirection issue")
+
+
+        time.sleep(3)
+
+
+
+    
 
     def tearDown(self):
         self.driver.quit
