fae4030b6e9cf55b1d0cbee0e95eb4c49deae613
Angie Restanio <arestani@uccs.edu>
2023-11-23 22:20:58 -0700

    modify test to have user register

--- inventory_app/tests/test_selenium.py
+++ inventory_app/tests/test_selenium.py
@@ -6,19 +6,34 @@ import os
 import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from django.contrib.auth.models import User, Group
+from inventory_app.models import Inventory, Owner
 
 SERVER_URL = 'http://127.0.0.1:8000'
 #SERVER_URL = self.live_server_url
 
 class Hosttest(LiveServerTestCase):
     def setUp(self):
-        #chrome_driver_path = "../chromedriver"
-        #chrome_driver_path = os.path.join(os.getcwd(), "chromedriver")
-        #os.environ["PATH"] += os.pathsep + chrome_driver_path
-        #self.driver = webdriver.Chrome(chrome_driver_path)
-        #os.environ["PATH"] += os.pathsep + chrome_driver_path
         self.driver = webdriver.Chrome()
+
+        '''
+        # register user
+        self.owner_user = User.objects.create_user(username='testusername', email='test@uccs.edu', password='testpassword')
+        self.owner_user.first_name = 'testname'
+        self.owner_user.save()
+
+        # Add the 'owner' group to the user
+        owner_group = Group.objects.create(name='owner') # create new group 'owner'
+        self.owner_user.groups.add(owner_group) # assocuate self.user_owner with 'owner' group
+
+        self.inventory = Inventory.objects.create()
+        self.owner = Owner.objects.create(user=self.owner_user, inventory=self.inventory)
+    
+        
         print('debug: ', type(self.driver))
+        print('User registered:', self.owner_user.username)
+        '''
+
 
 
     def get_server_url(self):
@@ -30,19 +45,39 @@ class Hosttest(LiveServerTestCase):
 
 
     def test_homepage(self):
-        print('TEST------ In test_homepage function')
+
         server_url = self.get_server_url()
-        #self.driver.get(self.live_server_url) 
         self.driver.get(server_url)
 
         self.assertIn("Inventory Tracker", self.driver.title)
-        #self.assertIn(1, [1,2,3])
         time.sleep(1)
+
+
+
 
     def test_login_logout(self):
         print('TEST------ In test_login_logout unction')
         server_url = self.get_server_url()
         print('TEST------ server_url = ', server_url)
+
+        self.driver.get(server_url  + '/accounts/register/')
+
+        username_input = self.driver.find_element(By.NAME, 'username')
+        email_input = self.driver.find_element(By.NAME, 'email')
+        name_input = self.driver.find_element(By.NAME, 'name')
+        password1_input = self.driver.find_element(By.NAME, 'password1')
+        password2_input = self.driver.find_element(By.NAME, 'password2')
+
+        username_input.send_keys('testusername')
+        email_input.send_keys('test@uccs.edu')
+        name_input.send_keys('testname')
+        password1_input.send_keys('testpassword')
+        password2_input.send_keys('testpassword')
+
+
+        submit_button = self.driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
+        submit_button.click()
+
 
         #self.driver.get(self.live_server_url + '/accounts/login/')
         self.driver.get(server_url  + '/accounts/login/')
@@ -55,21 +90,17 @@ class Hosttest(LiveServerTestCase):
         submit = self.driver.find_element(By.CSS_SELECTOR, 'input[type="submit"]')
 
         # log in user
-        user_name.send_keys('testuser')
-        user_password.send_keys('inventorytest')
+        user_name.send_keys('testusername')
+        user_password.send_keys('testpassword')
         submit.send_keys(Keys.RETURN)
 
-        # Explicitly wait for the welcome message
-        print("Current URL before waiting:", self.driver.current_url)
-        #wait = WebDriverWait(self.driver, 60)
-        time.sleep(10)
-        message = self.driver.find_element(By.XPATH, '//span[contains(text(), "Welcome, testuser")]')
-        #message = wait.until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(), "Welcome, testuser")]')))
+        time.sleep(3)
+        message = self.driver.find_element(By.XPATH, '//span[contains(text(), "Welcome, testusername")]')
         print("Current URL after waiting:", self.driver.current_url)
 
         # check for welcome message ensuring the correct user is logged in
-        
-        self.assertTrue(message.is_displayed(), "Welcome, testuser")
+        time.sleep(3)
+        self.assertTrue(message.is_displayed(), "Welcome, testusername")
 
         # log out user
         logout = self.driver.find_element(By.XPATH, '//a[@href="/accounts/logout/"]')
@@ -87,4 +118,15 @@ class Hosttest(LiveServerTestCase):
     
 
     def tearDown(self):
+        '''
+        # Clean up the test data after the test is complete
+        self.owner_user.delete()
+        self.owner.delete()
+        self.inventory.delete()
+
+        # Remove the user from the 'owner' group
+        owner_group = Group.objects.get(name='owner')
+        owner_group.user_set.remove(self.owner_user)
+        '''
+
         self.driver.quit
