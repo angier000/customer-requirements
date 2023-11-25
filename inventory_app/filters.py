8fb051627a016a05f6f9a5da18d3b9acc8dde571
Angie Restanio <arestani@uccs.edu>
2023-11-24 20:45:38 -0700

    finish basic search by name

--- inventory_app/filters.py
+++ inventory_app/filters.py
@@ -1,8 +1,11 @@
 import django_filters
 from .models import *
+from django_filters import CharFilter
 
 # fields for filters.serch
 class ItemFilter(django_filters.FilterSet):
+    name = CharFilter(field_name='name', lookup_expr='icontains')
+
     class Meta:
         model = Item
         fields = Itemfields = ('name', 'serial_number')
